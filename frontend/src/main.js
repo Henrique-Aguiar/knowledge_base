04b0a32b79aaa1d651399a11bf9b96abcab96a9f
Henrique-Aguiar <hrikelr@hotmail.com>
2022-08-08 17:45:33 -0300

    cadastro de artigos

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,9 +1,20 @@
+import 'font-awesome/css/font-awesome.css'
 import Vue from 'vue'
 
 import App from './App'
 
+import './config/bootstrap'
+import './config/msgs'
+import store from './config/store'
+import router from './config/router'
+
 Vue.config.productionTip = false
 
+// TEMPORARIO!
+require('axios').defaults.headers.common['Authorization'] = 'bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpZCI6MSwibmFtZSI6IkhlbnJpcXVlIEQiLCJlbWFpbCI6ImhoaGhoaEBnZ2dnLmNvbSIsImFkbWluIjp0cnVlLCJpYXQiOjE2NTk5OTEzMjUsImV4cCI6MTY2MDI1MDUyNX0.YlT8HNpEDzNt7q3g_KboW2b7OIc2fZHTpOyMeD7tIgI'
+
 new Vue({
+  store,
+  router,
   render: h => h(App)
 }).$mount('#app')
