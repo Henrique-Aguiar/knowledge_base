c14165fccb064fe88afcae6179a5be5a86380abd
Henrique-Aguiar <hrikelr@hotmail.com>
2022-08-10 20:42:38 -0300

    componente categoria

--- frontend/src/components/template/PageTitle.vue
+++ frontend/src/components/template/PageTitle.vue
@@ -1,14 +1,14 @@
 <template>
-  <div class="page-title">
-    <h1><i v-if="icon" :class="icon"></i> {{ main }}</h1>
-    <h2>{{ sub }}</h2>
-    <hr>
-  </div>
+    <div class="page-title">
+        <h1><i v-if="icon" :class="icon"></i> {{ main }}</h1>
+        <h2>{{ sub }}</h2>
+        <hr>
+    </div>
 </template>
 
 <script>
 export default {
-    name:"PageTitle",
+    name: 'PageTitle',
     props: ['icon', 'main', 'sub']
 }
 </script>
