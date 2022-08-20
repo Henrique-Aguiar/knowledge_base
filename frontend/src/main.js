db55d44b676d79b11dbe9eeccad9ff7d5fd0bfd8
Henrique-Aguiar <hrikelr@hotmail.com>
2022-08-20 19:23:47 -0300

    breakpoints

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -5,13 +5,13 @@ import App from './App'
 
 import './config/bootstrap'
 import './config/msgs'
+import './config/axios'
+import './config/mq'
+
 import store from './config/store'
 import router from './config/router'
 
 Vue.config.productionTip = false
-
-// TEMPORARIO!
-require('axios').defaults.headers.common['Authorization'] = 'bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpZCI6MSwibmFtZSI6IkhlbnJpcXVlIEQiLCJlbWFpbCI6ImhoaGhoaEBnZ2dnLmNvbSIsImFkbWluIjp0cnVlLCJpYXQiOjE2NTk5OTEzMjUsImV4cCI6MTY2MDI1MDUyNX0.YlT8HNpEDzNt7q3g_KboW2b7OIc2fZHTpOyMeD7tIgI'
 
 new Vue({
   store,
